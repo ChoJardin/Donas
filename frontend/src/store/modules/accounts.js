617892fc7fffd6a81e8e896e022d13c4a371e016
cho.jardin <cho.jardin@gmail.com>
2021-07-22 17:18:20 +0900

    vue_settings

---
+++ frontend/src/store/modules/accounts.js
@@ -0,0 +1,25 @@
+// import cookies from 'vue-cookies'
+// import DRF from '@/api/drf.js'
+// import axios from 'axios'
+// import router from '@/router'
+//
+
+const state ={
+
+}
+
+const getters = {
+
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+export default {
+    state, getters, mutations, actions
+}
