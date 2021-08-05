bd50e1231ee18320b2d005da20f2c1c69ae812e3
cho.jardin <cho.jardin@gmail.com>
2021-08-06 03:22:16 +0900

    #S05P13A603-71 [+] login/ signup page design change

--- frontend/src/views/user/Login.vue
+++ frontend/src/views/user/Login.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="login">
-    <h1>로그인</h1>
-    <br><br><br>
+    <ComponentNav @on-arrow="$router.back()"/>
+    <div id="title">
+      로그인
+    </div>
 
     <UserInput class="user-input"
         id="email" label="Email" placeholder="이메일을 입력하세요" type="email"
@@ -17,10 +19,29 @@
       {{ loginError.message }}
     </div>
 
-    <button class="button" :disabled="!isSubmit" @click="onLogin">로그인</button>
+    <button class="button" :disabled="!isSubmit" @click="onLogin">로 그 인</button>
 
-    <br><br><br>
-    <button @click="$router.push({name: 'Signup', params: $route.params})"> 회원가입 </button>
+    <!--소셜 로그인-->
+    <div id="login-social" class="login-description">
+      <hr>
+      <span style="margin-left: 8%">소셜 로그인</span>
+    </div>
+    <!--소셜 로그인-->
+
+    <!--to 회원가입-->
+    <div id="login-to-signup">
+      <div class="login-description">
+        <hr>
+        <span> 아직 회원이 아니신가요? </span>
+        <hr>
+      </div>
+
+    <ButtonBig
+        @on-click="$router.push({name: 'Signup', params: $route.params})"
+        value="회 원 가 입" color="#e1eedd" text="#183a1d" id="to-signup-button"/>
+    </div>
+    <!--to 회원가입-->
+
 
   </div>
 </template>
@@ -29,14 +50,18 @@
 import * as EmailValidator from "email-validator"
 import PV from "password-validator"
 import UserApi from "@/api/UserApi";
+import ComponentNav from "@/components/common/ComponentNav";
 import UserInput from "@/components/common/UserInput";
+import ButtonBig from "@/components/common/ButtonBig";
 import { token } from '@/services/messaging/messaging.ts';
 
 export default {
   name: "Login",
   // components
   components: {
-    UserInput
+    ComponentNav,
+    UserInput,
+    ButtonBig
   },
   // props
   // data
@@ -141,6 +166,14 @@ export default {
   flex-direction: column;
 }
 
+#login #title {
+  color: #183a1d;
+  margin-bottom: 30px;
+  padding: 10px 0;
+  font-family: GongGothicBold;
+  font-size: 1.2em;
+}
+
 .user-input {
   width: 90%;
   margin-left: 5%;
@@ -158,6 +191,7 @@ export default {
 .button {
   width: 90%;
   margin-left: 5%;
+  margin-top: 25px;
   height: 50px;
   background-color: #183a1d;
   color: #e1eedd;
@@ -171,5 +205,38 @@ export default {
   cursor: default;
 }
 
+/* 소셜로그인, 회원가입 */
+.login-description{
+  width: 90%;
+  margin-left: 5%;
+  font-family: GongGothicLight;
+  font-size: 0.9em;
+  color: #183a1d;
+}
 
+hr {
+  display: inline-block;
+  width: calc((100% - 200px) / 2)
+}
+
+#login-social {
+  display: flex;
+  align-items: center;
+  justify-items: start;
+  margin-top: 60px;
+}
+
+#login-to-signup .login-description{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 60px;
+  margin-bottom: 30px;
+}
+
+
+#to-signup-button {
+  width: 90%;
+  margin: auto;
+}
 </style>
