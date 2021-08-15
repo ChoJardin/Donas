8cf70c45e26821e683f574a1cc28d874f63169c0
rbdlf113 <rbdlf113@naver.com>
2021-08-15 13:05:36 +0900

    upa

--- frontend/src/api/UserApi.js
+++ frontend/src/api/UserApi.js
@@ -22,6 +22,10 @@ const ROUTES = {
   answerAlert: '/alarm/group',
   // declineAlert: '/alarm/group',
 
+  recentSearch: '/search/recent/',
+  searchAuto: '/search/auto',
+  searchResult: '/search/result',
+  saveSearch: '/search/add'
 
 }
 
@@ -40,6 +44,7 @@ const requestLogin = (data, callback, errorCallback) => {
 //   axios.get(requestLoginUserPath)
 //     .then(res => callback(res))
 //     .catch(err => errorcallback(err))
+
 // }
 
 const requestLoginUser = (id, callback, errorCallback) => {
@@ -174,7 +179,75 @@ const declineAlert = (data, callback, errorCallback) => {
     })
 }
 
+const kakaoLogin = async (code) => {
+  const Host = "https://kauth.kakao.com"
+  const tokenRequestPath = '/oauth/token'
+  const REST_API_KEY = '67852745621896a093fa2abcffbd1275'
+  // const REDIRECT_URI = 'https://i5a603.p.ssafy.io/login'
 
+  const header = {
+    'Authorization': '406aeac2c2a0e7bcd7940764bc4af504',
+    'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+  }
+
+  const data = {
+    grant_type: 'authorization_code',
+    client_id: REST_API_KEY,
+    redirect_uri: 'http://localhost:8080/login',
+    code: code
+  }
+  const queryString = Object.keys(data)
+    .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(data[k]))
+    .join('&')
+  console.log(queryString)
+  const result = await axios.post(Host + tokenRequestPath, queryString, { headers: header })
+  // .then(res=>console.log(res))
+  // .catch(err => console.log(err))
+  console.log(result)
+
+
+
+}
+
+const recentSearch = (id, callback, errorCallback) => {
+  const recentSearchPath = URL + ROUTES.recentSearch + id
+  axios.get(recentSearchPath)
+    .then(res => { callback(res) })
+    .catch(err => { errorCallback(err) })
+}
+
+const searchAuto = (data, callback, errorCallback) => {
+  const searchAutoPath = URL + ROUTES.searchAuto
+  axios.patch(searchAutoPath, null, { params: data })
+    .then(res => {
+      callback(res)
+    })
+    .catch(err => {
+      errorCallback(err)
+    })
+}
+
+const searchResult = (data, callback, errorCallback) => {
+  const searchResultPath = URL + ROUTES.searchAuto
+  axios.patch(searchResultPath, null, { params: data })
+    .then(res => {
+      callback(res)
+    })
+    .catch(err => {
+      errorCallback(err)
+    })
+}
+
+const saveSearch = (data, callback, errorCallback) => {
+  const saveSearchPath = URL + ROUTES.saveSearch
+  axios.patch(saveSearchPath, data)
+    .then(res => {
+      callback(res)
+    })
+    .catch(err => {
+      errorCallback(err)
+    })
+}
 
 const UserApi = {
   URL,
@@ -198,9 +271,12 @@ const UserApi = {
   acceptAlert: (data, callback, errorCallback) => acceptAlert(data, callback, errorCallback),
   declineAlert: (params, callback, errorCallback) => declineAlert(params, callback, errorCallback),
 
+  kakaoLogin: (code) => kakaoLogin(code),
 
+  recentSearch: (id, callback, errorCallback) => recentSearch(id, callback, errorCallback),
+  searchAuto: (data, callback, errorCallback) => searchAuto(data, callback, errorCallback),
+  searchResult: (data, callback, errorCallback) => searchResult(data, callback, errorCallback),
+  saveSearch: (data, callback, errorCallback) => saveSearch(data, callback, errorCallback),
 }
-
-
 
 export default UserApi
