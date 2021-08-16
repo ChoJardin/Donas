915393901bef07f555e34db42145713f7a55f2f5
multicampus <jooheejin95@gmail.com>
2021-08-16 12:36:19 +0900

    S05P13A603-193 [+] FCM error

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,17 +1,18 @@
 import firebase from 'firebase/app'
 import 'firebase/firebase-messaging'
 
-var firebaseConfig = {
-    apiKey: "AIzaSyAlkz45CmAwsU4sJhmjJjAoqHUWPMka8oE",
-    authDomain: "donas-d47d8.firebaseapp.com",
-    databaseURL: "https://donas-d47d8-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "donas-d47d8",
-    storageBucket: "donas-d47d8.appspot.com",
-    messagingSenderId: "635702943991",
-    appId: "1:635702943991:web:ea99975d129734b24ef3f2",
-    measurementId: "G-VJ7XLPCD90"
-}
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB4rMcDZZXyCgnnVaB5VD0F_Hzt0evwgyY",
+  authDomain: "donas-d47d8.firebaseapp.com",
+  databaseURL: "https://donas-d47d8-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "donas-d47d8",
+  storageBucket: "donas-d47d8.appspot.com",
+  messagingSenderId: "635702943991",
+  appId: "1:635702943991:web:ea99975d129734b24ef3f2",
+  measurementId: "G-VJ7XLPCD90"
+};
 
-firebase.initializeApp(firebaseConfig)
+// firebase.initializeApp(firebaseConfig)
 
-export default firebase.messaging()
+export default firebaseConfig
--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,15 +1,16 @@
 importScripts('https://www.gstatic.com/firebasejs/8.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.8.1/firebase-messaging.js');
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const config = {
-    apiKey: "AIzaSyAlkz45CmAwsU4sJhmjJjAoqHUWPMka8oE",
-    authDomain: "donas-d47d8.firebaseapp.com",
-    databaseURL: "https://donas-d47d8-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "donas-d47d8",
-    storageBucket: "donas-d47d8.appspot.com",
-    messagingSenderId: "635702943991",
-    appId: "1:635702943991:web:ea99975d129734b24ef3f2",
-    measurementId: "G-VJ7XLPCD90"
+  apiKey: "AIzaSyB4rMcDZZXyCgnnVaB5VD0F_Hzt0evwgyY",
+  authDomain: "donas-d47d8.firebaseapp.com",
+  databaseURL: "https://donas-d47d8-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "donas-d47d8",
+  storageBucket: "donas-d47d8.appspot.com",
+  messagingSenderId: "635702943991",
+  appId: "1:635702943991:web:ea99975d129734b24ef3f2",
+  measurementId: "G-VJ7XLPCD90"
 };
 
 firebase.initializeApp(config);
