c579c486759cf4b1c788e520ba2f5070e3c1b9a9
multicampus <jooheejin95@gmail.com>
2021-08-16 12:19:45 +0900

    S05P13A603-193 [+] FCM error

--- frontend/src/services/messaging/messaging.ts
+++ frontend/src/services/messaging/messaging.ts
@@ -18,7 +18,7 @@ import Config from '../../../firebase';
 
 // firebase.initializeApp(Config);
 
-const token = ref("");
+const token = ref("AAAAlALZIPc:APA91bFPmuJhhTAeltNqFrm5OpS0Y7FVJHTQrr685xhCdqZmM5QUVU3u1NAXp6NtgeVg0M6w88tDeGvL30aHOUIdL8o7nDQlT_Nr90CgVPjSpj6lGORaXmpaVBx6k4VQla_rsuP5ZVxL");
 const messaging = Config;
 messaging.usePublicVapidKey("BNtJjn9ch7RFp8N9V3C6SDBE02Zr-_W_OIoZSZcP6uT31Nlzsw22xpRbZ_EcX_04U0zjSePNFaF71aNG7tDZkMY");
 
