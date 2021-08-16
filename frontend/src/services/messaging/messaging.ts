4494215cc524b98228a42ef78551ef25819ac30a
multicampus <jooheejin95@gmail.com>
2021-08-16 12:09:20 +0900

    S05P13A603-193 [+] FCM error

--- frontend/src/services/messaging/messaging.ts
+++ frontend/src/services/messaging/messaging.ts
@@ -1,29 +1,31 @@
 import  {ref}  from '../../composition/index.ts';
 import firebase from 'firebase/app';
 import 'firebase/messaging';
+import Config from '../../../firebase';
+
 // import '@firebase/messaging';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const Config = {
-  apiKey: "AIzaSyAlkz45CmAwsU4sJhmjJjAoqHUWPMka8oE",
-  authDomain: "donas-d47d8.firebaseapp.com",
-  databaseURL: "https://donas-d47d8-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "donas-d47d8",
-  storageBucket: "donas-d47d8.appspot.com",
-  messagingSenderId: "635702943991",
-  appId: "1:635702943991:web:ea99975d129734b24ef3f2",
-  measurementId: "G-VJ7XLPCD90"
-};
+// const Config = {
+//   apiKey: "AIzaSyAlkz45CmAwsU4sJhmjJjAoqHUWPMka8oE",
+//   authDomain: "donas-d47d8.firebaseapp.com",
+//   databaseURL: "https://donas-d47d8-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "donas-d47d8",
+//   storageBucket: "donas-d47d8.appspot.com",
+//   messagingSenderId: "635702943991",
+//   appId: "1:635702943991:web:ea99975d129734b24ef3f2",
+//   measurementId: "G-VJ7XLPCD90"
+// };
 
-firebase.initializeApp(Config);
+// firebase.initializeApp(Config);
 
-const token = ref("AAAAlALZIPc:APA91bFPmuJhhTAeltNqFrm5OpS0Y7FVJHTQrr685xhCdqZmM5QUVU3u1NAXp6NtgeVg0M6w88tDeGvL30aHOUIdL8o7nDQlT_Nr90CgVPjSpj6lGORaXmpaVBx6k4VQla_rsuP5ZVxL");
-const messaging = firebase.messaging();
+const token = ref("");
+const messaging = Config;
 messaging.usePublicVapidKey("BNtJjn9ch7RFp8N9V3C6SDBE02Zr-_W_OIoZSZcP6uT31Nlzsw22xpRbZ_EcX_04U0zjSePNFaF71aNG7tDZkMY");
 
 
 messaging.getToken().then((currentToken) => {
     if (currentToken) {
-        // console.log(currentToken);
+        console.log(currentToken);
         token.value = currentToken;
     } else {
         // Show permission request.
