637d0b4dbd47812b535ceb8590edab279fce9ec5
rbdlf113 <rbdlf113@naver.com>
2021-08-17 14:47:07 +0900

    Add chat-service(chat domain)

---
+++ backend/src/main/java/com/ssafy/donas/domain/Chat.java
@@ -0,0 +1,48 @@
+package com.ssafy.donas.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class Chat {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "content")
+	private String content;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "send_id")
+	private User sendUser;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "receive_id")
+	private User receivedUser;
+	
+	@Column(nullable = false, updatable = false)
+	@ColumnDefault("CURRENT_TIMESTAMP()")
+	private LocalDateTime time;	
+	
+}
---
+++ backend/src/main/java/com/ssafy/donas/service/ChatService.java
@@ -0,0 +1,20 @@
+package com.ssafy.donas.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ChatService {
+	
+	
+	// 채팅방 리스트
+	
+	// 채팅 보내기
+	
+	// 채팅방 들어가기
+	
+	//
+
+}
