f0b82c031d910b6b17c2097ca5ae217e1b66735c
rbdlf113 <rbdlf113@naver.com>
2021-08-17 21:15:39 +0900

    udpate

--- backend/src/main/java/com/ssafy/donas/domain/Messageroom.java
+++ backend/src/main/java/com/ssafy/donas/domain/Messageroom.java
@@ -1,57 +1,57 @@
-package com.ssafy.donas.domain;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-
-@Entity
-@Data
-@AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@Builder
-public class Messageroom {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "userone_id")
-	private User user1;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "usertwo_id")
-	private User user2;
-	
-	@OneToMany(mappedBy = "messageroom", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	@JsonIgnore
-	private List<Message> msg = new ArrayList<>();
-	
-	public Messageroom() {}
-	
-	@Builder
-	public Messageroom(User user1, User user2) {
-
-		this.user1 = user1;
-		this.user2 = user2;
-	}
-	
-	
-}
+//package com.ssafy.donas.domain;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import javax.persistence.CascadeType;
+//import javax.persistence.Entity;
+//import javax.persistence.FetchType;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.JoinColumn;
+//import javax.persistence.ManyToOne;
+//import javax.persistence.OneToMany;
+//import javax.persistence.Table;
+//
+//
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.fasterxml.jackson.annotation.JsonInclude;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//
+//@Entity
+//@Data
+//@AllArgsConstructor
+//@JsonInclude(JsonInclude.Include.NON_NULL)
+//@Builder
+//public class Messageroom {
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private long id;
+//	
+//	@ManyToOne(fetch = FetchType.LAZY)
+//	@JoinColumn(name = "userone_id")
+//	private User user1;
+//	
+//	@ManyToOne(fetch = FetchType.LAZY)
+//	@JoinColumn(name = "usertwo_id")
+//	private User user2;
+//	
+//	@OneToMany(mappedBy = "messageroom", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+//	@JsonIgnore
+//	private List<Message> msg = new ArrayList<>();
+//	
+//	public Messageroom() {}
+//	
+//	@Builder
+//	public Messageroom(User user1, User user2) {
+//
+//		this.user1 = user1;
+//		this.user2 = user2;
+//	}
+//	
+//	
+//}
--- backend/src/main/java/com/ssafy/donas/repository/MessageRoomRepo.java
+++ backend/src/main/java/com/ssafy/donas/repository/MessageRoomRepo.java
@@ -1,12 +1,12 @@
-package com.ssafy.donas.repository;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ssafy.donas.domain.Messageroom;
-import com.ssafy.donas.domain.User;
-
-public interface MessageRoomRepo extends JpaRepository<Messageroom, Long>{
-	Messageroom findMessageRoomByUser1AndUser2(User user1, User user2);
-}
+//package com.ssafy.donas.repository;
+//
+//import java.util.Optional;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.ssafy.donas.domain.Messageroom;
+//import com.ssafy.donas.domain.User;
+//
+//public interface MessageRoomRepo extends JpaRepository<Messageroom, Long>{
+//	Messageroom findMessageRoomByUser1AndUser2(User user1, User user2);
+//}
