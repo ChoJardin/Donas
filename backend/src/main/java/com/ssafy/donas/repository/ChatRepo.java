04e498bd21600fafa77974592ce065c6f858ceb7
rbdlf113 <rbdlf113@naver.com>
2021-08-17 15:32:39 +0900

    add find chat

--- backend/src/main/java/com/ssafy/donas/repository/ChatRepo.java
+++ backend/src/main/java/com/ssafy/donas/repository/ChatRepo.java
@@ -9,6 +9,6 @@ import com.ssafy.donas.domain.User;
 
 public interface ChatRepo extends JpaRepository<Chat, Long>{
 	
-	List<Chat> findChatBySend_idAndReceived_id(long sendId, long receivedId);
+	List<Chat> findChatBySendUserAndReceivedUser(User sendUser, User receivedUser);
 
 }
